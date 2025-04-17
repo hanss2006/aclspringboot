abbd8614235b1a0f561241454efddbba9fd6f226
pgrudanov <hanss@list.ru>
2025-04-17 23:28:55 +0300

    Initial commit

--- aclback/src/main/java/com/ocrv/skimrv/backend/conf/SecurityConfig.java
+++ aclback/src/main/java/com/ocrv/skimrv/backend/conf/SecurityConfig.java
@@ -56,5 +56,4 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
--- aclback/src/main/resources/acr.sql
+++ aclback/src/main/resources/acr.sql
@@ -32,3 +32,32 @@ CREATE TABLE acl_entry (
     FOREIGN KEY (acl_object_identity) REFERENCES acl_object_identity(id),
     FOREIGN KEY (sid) REFERENCES acl_sid(id)
 );
+
+INSERT INTO acl_class (id, class) VALUES
+    (1, 'com.ocrv.skimrv.backend.dictionaries.entities.simple.OrgUnitDictionary');
+INSERT INTO acl_class (id, class) VALUES
+    (2, 'com.ocrv.skimrv.backend.dictionaries.entities.skim.DictRate');
+INSERT INTO acl_class (id, class) VALUES
+    (3, 'com.ocrv.skimrv.backend.dictionaries.entities.asfp.DictRateAsfp');
+
+INSERT INTO ACL_SID (PRINCIPAL, SID) VALUES (false, 'ROLE_ADMIN');
+INSERT INTO ACL_SID (PRINCIPAL, SID) VALUES (false, 'ROLE_USER');
+
+INSERT INTO acl_object_identity (id, object_id_class, object_id_identity, parent_object, owner_sid, entries_inheriting) VALUES
+    (1, 1, 1, NULL, 1, 0),
+    (2, 1, 2, NULL, 1, 0),
+    (3, 1, 3, NULL, 1, 0),
+    (4, 1, 4, NULL, 1, 0),
+    (5, 1, 5, NULL, 1, 0),
+
+    (6, 2, 1, NULL, 1, 0),
+    (7, 2, 2, NULL, 1, 0),
+    (8, 2, 3, NULL, 1, 0),
+    (9, 2, 4, NULL, 1, 0),
+    (10, 2, 5, NULL, 1, 0),
+
+    (11, 3, 1, NULL, 1, 0),
+    (12, 3, 2, NULL, 1, 0),
+    (13, 3, 3, NULL, 1, 0),
+    (14, 3, 4, NULL, 1, 0),
+    (15, 3, 5, NULL, 1, 0);
